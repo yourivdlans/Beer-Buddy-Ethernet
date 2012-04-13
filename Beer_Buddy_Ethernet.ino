1224f6018e7665bad73d04fd4d20aec17e9e6b8c
Youri van der Lans <youri@itflows.nl>
2012-04-13 17:25:26 +0200

    Added ethernet functionality.
    - When state is "I" look for a state.
    - When state is "R" look for new input (not working yet, empty previous buffer first).

--- Beer_Buddy_Ethernet.ino
+++ Beer_Buddy_Ethernet.ino
@@ -1,4 +1,18 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <Servo.h>
+
 #include <SoftwareSerial.h>
+#include <BeerBuddyEthernet.h>
+
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x03, 0x05 };
+IPAddress ip(85,214,92,47);
+char host[] = "http://beer-buddy.nl";
+char received;
+char state = 'I';
+char* rfid;
+
+BeerBuddyEthernet ethernet(mac, ip, host);
 
 SoftwareSerial RFModule(2, 3);
 
@@ -6,14 +20,75 @@ void setup()
 {
   Serial.begin(9600);
   RFModule.begin(9600);
+  
+  ethernet.initialize();
+  ethernet.setOnline();
 }
 
 void loop()
 {
-  if (RFModule.available()) {
-    Serial.print((char)RFModule.read());
+  if ( state = 'I' )
+  {
+    // Receive
+    if ( RFModule.available() )
+    {
+      received = (char)RFModule.read();
+      Serial.println(received);
+      lookForRfid(received);
+      received = 0;
+    }
+    // Transmit
+    if ( Serial.available() )
+    {
+      received = (char)Serial.read();
+      RFModule.println(received);
+      lookForRfid(received);
+      received = 0;
+    }
   }
-  if (Serial.available()) {
-    RFModule.print((char)Serial.read());
+  if ( state == 'R' )
+  {
+    if ( RFModule.available() )
+    {
+      received = (char)RFModule.read();
+      rfid = ethernet.sendRFID(&received);
+      state = 'I';
+    }
+    if ( Serial.available() )
+    {
+      received = (char)Serial.read();
+      rfid = ethernet.sendRFID(&received);
+      state = 'I';
+    }
   }
+}
+
+void lookForRfid(char input)
+{
+  if ( input == 'R' )
+  {
+    ethernet.keepAlive();
+    
+    state = 'R';
+  }
+}
+
+boolean getSerialValue()
+{
+  // Receive
+  if ( RFModule.available() )
+  {
+    received = (char)RFModule.read();
+    Serial.println(received);
+    return true;
+  }
+  // Transmit
+  if ( Serial.available() )
+  {
+    received = (char)Serial.read();
+    RFModule.println(received);
+    return true;
+  }
+  
+  return false;
 }
