bc8e528398f50beea24c710d0e5940157ec82279
Youri van der Lans <youri@itflows.nl>
2012-04-25 11:47:14 +0200

    Finished the Idle and Rfid state.
    - When in idle state will look for state change and set state.
    - When in Rfid state will wait for the buffer to fill to 12 and then do the sendRFID request.

--- Beer_Buddy_Ethernet.ino
+++ Beer_Buddy_Ethernet.ino
@@ -6,13 +6,18 @@
 #include <BeerBuddyEthernet.h>
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x03, 0x05 };
-IPAddress ip(85,214,92,47);
+IPAddress remote_ip(85,214,92,47);
+IPAddress local_ip(145,52,201,100);
 char host[] = "http://beer-buddy.nl";
 char received;
+// I = Idle, R = RFID, A = Alcohol
 char state = 'I';
-char* rfid;
+char rfid[12];
+char* user;
+int i;
+int y;
 
-BeerBuddyEthernet ethernet(mac, ip, host);
+BeerBuddyEthernet ethernet(mac, remote_ip, host);
 
 SoftwareSerial RFModule(2, 3);
 
@@ -21,74 +26,81 @@ void setup()
   Serial.begin(9600);
   RFModule.begin(9600);
   
+  ethernet.setLocalIp(local_ip);
   ethernet.initialize();
   ethernet.setOnline();
 }
 
 void loop()
 {
-  if ( state = 'I' )
+  if ( state == 'I' )
   {
-    // Receive
-    if ( RFModule.available() )
-    {
-      received = (char)RFModule.read();
-      Serial.println(received);
-      lookForRfid(received);
-      received = 0;
-    }
-    // Transmit
-    if ( Serial.available() )
-    {
-      received = (char)Serial.read();
-      RFModule.println(received);
-      lookForRfid(received);
-      received = 0;
-    }
-  }
-  if ( state == 'R' )
-  {
-    if ( RFModule.available() )
-    {
-      received = (char)RFModule.read();
-      rfid = ethernet.sendRFID(&received);
-      state = 'I';
-    }
-    if ( Serial.available() )
-    {
-      received = (char)Serial.read();
-      rfid = ethernet.sendRFID(&received);
-      state = 'I';
-    }
-  }
-}
-
-void lookForRfid(char input)
-{
-  if ( input == 'R' )
-  {
-    ethernet.keepAlive();
+    i = 0;
     
-    state = 'R';
-  }
-}
-
-boolean getSerialValue()
-{
-  // Receive
-  if ( RFModule.available() )
-  {
-    received = (char)RFModule.read();
-    Serial.println(received);
-    return true;
-  }
-  // Transmit
-  if ( Serial.available() )
-  {
-    received = (char)Serial.read();
-    RFModule.println(received);
-    return true;
+    if ( Serial.available() == 1 )
+    {
+      while ( Serial.available() )
+      {
+        received = (char)Serial.read();
+        
+        if ( i == 0 && received == 'R' )
+        {
+          // Check if server is online
+          Serial.println("1");
+          state = 'R';
+        }
+        
+        ++i;
+      }
+    }
   }
   
-  return false;
+  if ( state == 'R' )
+  {
+    i = 0;
+    y = 0;
+    
+    if ( Serial.available() == 12 )
+    {
+      while ( Serial.available() )
+      {
+        received = (char)Serial.read();
+        
+        Serial.print("I: ");
+        Serial.print(i);
+        Serial.print(" - ");
+        Serial.print("Received: ");
+        Serial.print(received);
+        
+        if ( i != 0 && i <= 12 )
+        {
+          Serial.print(" <= added to rfid[");
+          Serial.print(y);
+          Serial.print("]");
+          
+          rfid[y] = received;
+          ++y;
+          
+          if ( i == 12 )
+          {
+            rfid[y] = '\0';
+            
+            //Serial.print("Y: ");
+            //Serial.print(y);
+            //Serial.println();
+          }
+        }
+        
+        Serial.println();
+        
+        ++i;
+      }
+      
+      user = ethernet.sendRFID(rfid);
+      
+      Serial.println(user);
+      
+      state = 'I';
+    }
+  }
 }
